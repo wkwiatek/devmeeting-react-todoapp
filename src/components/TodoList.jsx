aa3e1f26cb60681d02fbae51ee32bfa52cdbfb9a
Michał Załęcki <zalecki.post@gmail.com>
2016-01-16 20:53:24 +0100

    Add props types to TodoList to make it easier to reuse

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 class TodoList extends React.Component {
+  static propTypes = {
+    todos: React.PropTypes.arrayOf(React.PropTypes.string),
+  };
+
   render() {
     return (
       <ul>{ this.props.todos.map(todo => <li key={todo}>{ todo }</li>) }</ul>
