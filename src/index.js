07e08e6b8b9256867017d09950fa9687a61d1b22
Michał Załęcki <zalecki.post@gmail.com>
2016-01-16 20:32:40 +0100

    Add form and todo list to App component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World!</h1>
+    return (
+      <div>
+        <form>
+          <input type="text" name="todo" />
+          <button>Add Todo</button>
+        </form>
+        <ul>
+          <li>Todo 1</li>
+          <li>Todo 2</li>
+          <li>Todo 3</li>
+        </ul>
+      </div>
+    );
   }
 }
 
