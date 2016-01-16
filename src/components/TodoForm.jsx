bbed7971959f9050e70772cb12482917b9863a02
Michał Załęcki <zalecki.post@gmail.com>
2016-01-16 21:11:36 +0100

    Handle input change and form submission

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.todo);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" name="todo" />
-        <button>Add Todo</button>
+      <form onSubmit={ ::this.handleSubmit }>
+        <input
+          type="text"
+          name="todo"
+          onChange={ ::this.handleChange }
+          value={ this.state.todo }
+        />
+        <input type="submit" value="Add Todo" />
       </form>
     );
   }
