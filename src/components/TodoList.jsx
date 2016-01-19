bf0a70f85251650149a723c8d22b44227bf1a9a3
Michał Załęcki <zalecki.post@gmail.com>
2016-01-19 23:25:18 +0100

    Create TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div className="todo-list">
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+          <li>Task 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
