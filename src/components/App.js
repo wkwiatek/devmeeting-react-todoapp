2d1434a7959fb7242615011935c810675f01791f
Michał Załęcki <zalecki.post@gmail.com>
2016-01-19 23:23:41 +0100

    Exchange markup for 2 components

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <form>
-          <input type="text" /><input type="submit"/>
-        </form>
-        <ul>
-          <li>Task 1</li>
-          <li>Task 2</li>
-          <li>Task 3</li>
-        </ul>
+        <TodoForm />
+        <TodoList />
       </div>
     );
   }
