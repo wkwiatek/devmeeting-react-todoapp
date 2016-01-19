c77643d410f50aa6f1c7912214894541b83741f3
Michał Załęcki <zalecki.post@gmail.com>
2016-01-19 23:34:40 +0100

    Pass todos from App to TodoList using props

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 class TodoList extends React.Component {
   render() {
-    const todos = [ "Task 1", "Task 2", "Task 3" ];
     return (
       <div className="todo-list">
-        <ul>{ todos.map(todo => <li key={ todo }>{ todo }</li>) }</ul>
+        <ul>{ this.props.todos.map(todo =><li key={ todo }>{ todo }</li>) }</ul>
       </div>
     );
   }
