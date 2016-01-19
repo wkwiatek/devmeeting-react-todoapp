722ffbd7a886f091fc96683ad2bd05e83a43b71c
Michał Załęcki <zalecki.post@gmail.com>
2016-01-19 23:59:40 +0100

    Handle input change

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 class TodoForm extends React.Component {
+  handleChange(e) {
+    console.log(e.target.name, e.target.value);
+  }
+
   render() {
     return (
       <div className="todo-form">
         <form>
-          <input type="text" /><input type="submit"/>
+          <input type="text" name="todo" onChange={ ::this.handleChange } />
+          <input type="submit" />
         </form>
       </div>
     );
