1fd10d25f03963557c7eea2d04b85f208de3da1e
Michał Załęcki <zalecki.post@gmail.com>
2016-01-20 02:42:48 +0100

    Apply thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "./middleware/logger";
 import todos from "./reducers/todos";
+import thunk from "redux-thunk";
 
-const store = applyMiddleware(logger)(createStore)(combineReducers({ todos }));
+const store = applyMiddleware(thunk, logger)(createStore)(combineReducers({ todos }));
 
 export default store;
