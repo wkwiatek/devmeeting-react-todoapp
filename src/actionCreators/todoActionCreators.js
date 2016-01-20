ed4b2d8631f8e4d683796e8bd69146b14f621c2e
Michał Załęcki <zalecki.post@gmail.com>
2016-01-20 02:37:51 +0100

    Add fetch todos actions

--- src/actionCreators/todoActionCreators.js
+++ src/actionCreators/todoActionCreators.js
@@ -1,10 +1,44 @@
+import axios from "axios";
 import {
   ADD_TODO,
+  FETCH_TODOS_REQUEST,
+  FETCH_TODOS_SUCCESS,
+  FETCH_TODOS_FAILURE,
 } from "../actionTypes/todoActionTypes";
 
 export function addTodo(todo) {
   return {
     type: ADD_TODO,
     payload: { todo },
+  };
+}
+
+function fetchTodosRequest() {
+  return {
+    type: FETCH_TODOS_REQUEST,
+  };
+}
+
+function fetchTodosSuccess(todos) {
+  return {
+    type: FETCH_TODOS_SUCCESS,
+    payload: { todos },
+  };
+}
+
+function fetchTodosFailure(error) {
+  return {
+    type: FETCH_TODOS_FAILURE,
+    payload: error,
+    error: true,
+  };
+}
+
+export function fetchTodos() {
+  return dispatch => {
+    dispatch(fetchTodosRequest());
+    axios.get("/todos.json")
+      .then(({ data }) => dispatch(fetchTodosSuccess(data)))
+      .catch((error) => dispatch(fetchTodosFailure(error)));
   };
 }
