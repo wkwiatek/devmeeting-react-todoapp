2e5397f335e8dcaae98bc2ed6997a808c6b85215
Michał Załęcki <zalecki.post@gmail.com>
2016-01-20 02:40:23 +0100

    Update reducer to handle fetched todos

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,11 +1,14 @@
 import {
   ADD_TODO,
+  FETCH_TODOS_SUCCESS,
 } from "../actionTypes/todoActionTypes";
 
 export default function todos(state = [], action) {
   switch(action.type) {
     case ADD_TODO:
       return [...state, action.payload.todo];
+    case FETCH_TODOS_SUCCESS:
+      return action.payload.todos;
     default:
       return state;
   }
