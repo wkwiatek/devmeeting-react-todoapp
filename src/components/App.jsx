c1a78d82a3a5429799ba88b1ff1fcb57864ca0f8
Michał Załęcki <zalecki.post@gmail.com>
2016-01-20 03:31:16 +0100

    Reduce even more App boilerplate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
-import store from "../store";
 import { connect } from "react-redux";
-import { addTodo, fetchTodos } from "../actionCreators/todoActionCreators";
+import { bindActionCreators } from "redux";
+import * as actions from "../actionCreators/todoActionCreators";
 
 class App extends React.Component {
   componentDidMount() {
-    store.dispatch(fetchTodos());
+    this.props.fetchTodos();
   }
 
   render() {
     return (
       <div className="app">
-        <TodoForm newTodo={ (todo) => store.dispatch(addTodo(todo)) } />
+        <TodoForm newTodo={ this.props.addTodo } />
         <TodoList todos={ this.props.todos } />
       </div>
     );
   }
 }
 
-export default connect(state => state)(App);
+export default connect(state => state, dispatch => ({
+  ...bindActionCreators(actions, dispatch)
+}))(App);
