99eb4072419567191dec9d27dcf8e5aa324f7463
Michał Załęcki <zalecki.post@gmail.com>
2016-01-20 03:13:13 +0100

    Reduce App boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,22 @@ import React from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import store from "../store";
+import { connect } from "react-redux";
 import { addTodo, fetchTodos } from "../actionCreators/todoActionCreators";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = store.getState();
-  }
-
   componentDidMount() {
-    this.unsubscribe = store.subscribe(::this.onChange);
-
     store.dispatch(fetchTodos());
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  onChange() {
-    this.setState(store.getState());
   }
 
   render() {
     return (
       <div className="app">
         <TodoForm newTodo={ (todo) => store.dispatch(addTodo(todo)) } />
-        <TodoList todos={ this.state.todos } />
+        <TodoList todos={ this.props.todos } />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => state)(App);
