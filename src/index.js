473d17b9e165d109be09517b6a357b65a2dfbc4e
Michał Załęcki <zalecki.post@gmail.com>
2016-01-22 01:46:46 +0100

    Add routing
    Conflicts:
    	src/index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React    from "react";
 import ReactDOM from "react-dom";
 import App      from "./components/App";
 import store    from "./store";
+import routes   from "./routes";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Provider store={ store }><App /></Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(routes, document.getElementById("root"));
