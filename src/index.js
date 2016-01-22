00528ecf07e24a3f74fac2b59de40bc97f39f972
Michał Załęcki <zalecki.post@gmail.com>
2016-01-22 01:46:46 +0100

    Add routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React    from "react";
 import ReactDOM from "react-dom";
-import App      from "app/components/App";
-import store    from "./store";
-import { Provider } from "react-redux";
+import routes   from "./routes";
 
-ReactDOM.render(
-  <Provider store={ store }><App /></Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(routes, document.getElementById("root"));
